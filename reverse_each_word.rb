d6de118c4e4c54abce6f7d95302137fdee776e1c
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 19:52:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence1)
   new_array = []
   sentence1 = sentence1.split(' ')
 
-  # sentence1.each{|word| word.reverse << new_array}
-  # new_array.join(' ')
-  # return new_array
+  sentence1.each{|word| word.to_s.reverse << new_array}
+  new_array.join(' ')
+  return new_array
 
   # sentence1.each{|word|
   #   word = word.reverse
