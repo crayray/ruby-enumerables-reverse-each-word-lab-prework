209adaafcfd8e492b75bb3dc70cfae3562c23ddf
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 19:47:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,13 +4,13 @@ def reverse_each_word(sentence1)
 
   sentence1.each{|word| word.reversed << new_array}
   return new_array
-  
+
   # sentence1.each{|word|
   #   word = word.reverse
   #   new_array.unshift(word)}
 
 
-  
+
   # return new_array.join('')
   # array.each{|word| word.reverse}
 
