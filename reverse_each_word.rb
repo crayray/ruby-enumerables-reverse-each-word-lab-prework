cb4380f711a7bdd0782769e77c219609c5a8ffbf
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 19:29:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(sentence1)
-  array = sentence1.split
+  new_array = []
+  sentence1 = sentence1.split
+
+  sentence1.each{|word|new_array.unshift(word)}
+  return new_array.join('')
   # array.each{|word| word.reverse}
 
 end
